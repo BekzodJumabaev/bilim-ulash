118074433cb8f2b890735df76537970061551cdd
BekzodJumabaev <bekzodjumabaev04@gmail.com>
2026-04-28 18:50:48 +0500

    o'zgardi html lar

--- src/main/java/com/example/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/com/example/exceptions/GlobalExceptionsHandler.java
@@ -1,14 +1,26 @@
 package com.example.exceptions;
 
+import com.example.dto.userDto.UserDto;
+import com.example.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @ControllerAdvice
 public class GlobalExceptionsHandler {
+
+    private final UserService userService;
+
+    public GlobalExceptionsHandler(UserService userService) {
+        this.userService = userService;
+    }
 
     @ExceptionHandler(MyProjectException.class)
     public Object handleException(MyProjectException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
@@ -22,6 +34,14 @@ public class GlobalExceptionsHandler {
                 new ErrorMessage(ex.getErrorType().getMessage(), ex.getErrorType().getCode()),
                 HttpStatus.valueOf(ex.getErrorType().getCode())
         );
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model, Principal principal) {
+        if (principal != null) {
+            UserDto user = userService.findByPhoneNumber(principal.getName());
+            model.addAttribute("user", user);
+        }
     }
 }
 
